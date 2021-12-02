7c6dbc4c2fb63e143d8f042cc3089715d2b3db97
a-xai <akylaib007@gmail.com>
2021-12-03 01:29:17 +0600

    Init project, add main activity and 3 fragments (Calc, Exchange Rate, Unit Converter)

---
+++ app/src/main/java/io/portfolio/calculatorapp/MainActivity.java
@@ -0,0 +1,54 @@
+package io.portfolio.calculatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    FragmentAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager2 = findViewById(R.id.view_pager2);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        adapter = new FragmentAdapter(fragmentManager, getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Calculator"));
+        tabLayout.addTab(tabLayout.newTab().setText("Exchange Rate"));
+        tabLayout.addTab(tabLayout.newTab().setText("Unit Converter"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+
+    }
+}
